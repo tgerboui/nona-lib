284c26f2e6493434fed4a2817f8428bd994fa357
Thomas Gerbouin <thomasgerbouin@gmail.com>
2024-03-20 16:44:31 -1000

    feat(rpc-consumer): Better handle of rpc error

--- lib/modules/rpc-consumer/rpc-consumer.ts
+++ lib/modules/rpc-consumer/rpc-consumer.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 import { Rpc } from '../../services/rpc/rpc';
+import { rpcError } from './rpc-consumer-schema';
 
 export class RpcConsummer {
   rpc: Rpc;
@@ -8,10 +9,18 @@ export class RpcConsummer {
     this.rpc = rpc;
   }
 
+  // TODO: Create class for errors
   parseHandler<T>(data: unknown, schema: z.Schema<T>): T {
     try {
       return schema.parse(data);
     } catch (error) {
+      if (error instanceof z.ZodError) {
+        // Rpc error
+        const parsedError = rpcError.safeParse(data);
+        if (parsedError.success) {
+          throw new Error(parsedError.data.error);
+        }
+      }
       // TODO: Better error handling
       throw new Error('Response format');
     }
