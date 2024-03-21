aca781a5036942f57db688fd584b1890a3f3cf9f
Thomas Gerbouin <thomasgerbouin@gmail.com>
2024-03-20 16:43:42 -1000

    fix(key): Use key service to get pub/account ley
    Also change key names to avoid reserved word js error

---
+++ lib/modules/key/key-interface.ts
@@ -0,0 +1,6 @@
+// Must change the key names to avoid 'private' and 'public' reserved word error
+export interface AccountKeys {
+  publicKey: string;
+  privateKey: string;
+  account: string;
+}
--- lib/modules/key/key.ts
+++ lib/modules/key/key.ts
@@ -1,18 +1,26 @@
+import { KeyService } from '../../services/hash/key-service';
 import { RpcConsummer } from '../rpc-consumer/rpc-consumer';
+import { AccountKeys } from './key-interface';
 import { Keys } from './key-schema';
 
 export class Key extends RpcConsummer {
-  async create(): Promise<Keys> {
+  async create(): Promise<AccountKeys> {
     const keys = await this.rpc.call('key_create');
 
-    return this.parseHandler(keys, Keys);
+    const parsedKeys = this.parseHandler(keys, Keys);
+
+    return {
+      privateKey: parsedKeys.private,
+      publicKey: parsedKeys.public,
+      account: parsedKeys.account,
+    };
   }
 
-  async expand(key: string): Promise<Keys> {
-    const expandedKey = await this.rpc.call('key_expand', {
-      key,
-    });
-
-    return this.parseHandler(expandedKey, Keys);
+  async expand(privateKey: string): Promise<AccountKeys> {
+    return {
+      privateKey: privateKey,
+      publicKey: KeyService.getPublicKey(privateKey),
+      account: KeyService.getAccount(KeyService.getPublicKey(privateKey)),
+    };
   }
 }
