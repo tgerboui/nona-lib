cd60b3f3a12dba2aa3cd0c0f9b149a05ff0d30db
Thomas Gerbouin <thomasgerbouin@gmail.com>
2024-04-09 16:47:16 +0200

    doc(node): Add documentation on node

--- lib/modules/node/node-schema.ts
+++ lib/modules/node/node-schema.ts
@@ -13,21 +13,37 @@ export const Version = z.object({
 export type Version = z.infer<typeof Version>;
 
 export const Telemetry = z.object({
+  /** Average count of blocks in ledger (including unconfirmed) */
   block_count: z.string(),
+  /** Average count of blocks cemented in ledger (only confirmed) */
   cemented_count: z.string(),
+  /** Average count of unchecked blocks. This should only be considered an estimate as nodes running RocksDB may not return exact counts. */
   unchecked_count: z.string(),
+  /** Average count of accounts in ledger */
   account_count: z.string(),
+  /** 0 = unlimited; the mode is chosen if there is more than 1 common result otherwise the results are averaged (excluding 0) */
   bandwidth_cap: z.string(),
+  /** Average count of peers nodes are connected to */
   peer_count: z.string(),
+  /** Most common protocol version */
   protocol_version: z.string(),
+  /** Average number of seconds since the UTC epoch at the point where the response is sent from the peer */
   uptime: z.string(),
+  /** Mode of genesis block hashes */
   genesis_block: z.string(),
+  /** Most common major version */
   major_version: z.string(),
+  /** Most common minor version */
   minor_version: z.string(),
+  /** Most common patch version */
   patch_version: z.string(),
+  /** Most common pre release version */
   pre_release_version: z.string(),
+  /** Most common, meant for third party node software implementing the protocol so that it can be distinguished, 0 = Nano Foundation, 1 = Nano Foundation pruned node */
   maker: z.string(),
+  /** Number of milliseconds since the UTC epoch at the point where the response is sent from the peer */
   timestamp: z.string(),
+  /** Minimum network difficulty due to deprecated active difficulty measurements */
   active_difficulty: z.string(),
 });
 export type Telemetry = z.infer<typeof Telemetry>;
--- lib/modules/node/node.ts
+++ lib/modules/node/node.ts
@@ -1,18 +1,33 @@
 import { RpcConsummer } from '../rpc-consumer/rpc-consumer';
 import { Telemetry, Version, uptimeSchema } from './node-schema';
 
+/**
+ * Handle all node and network related operations
+ * @class
+ */
 export class Node extends RpcConsummer {
+  /**
+   * Return metrics from other nodes on the network.
+   * Summarized view of the whole network.
+   */
   public async telemetry(): Promise<Telemetry> {
     const res = await this.rpc.call('telemetry');
     return this.parseHandler(res, Telemetry);
   }
 
-  /// Return the number of seconds the node has been running
-  public async uptime(): Promise<string> {
+  /**
+   * Return node uptime in seconds
+   */
+  public async uptime(): Promise<number> {
     const res = await this.rpc.call('uptime');
-    return this.parseHandler(res, uptimeSchema).seconds;
+
+    const seconds = this.parseHandler(res, uptimeSchema).seconds;
+    return parseInt(seconds, 10);
   }
 
+  /**
+   * Returns version information for RPC, Store, Protocol (network), Node (Major & Minor version).
+   */
   public async version(): Promise<Version> {
     const res = await this.rpc.call('version');
     return this.parseHandler(res, Version);
