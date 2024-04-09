f99d100973204054621b324ec882e32aff8d730b
Thomas Gerbouin <thomasgerbouin@gmail.com>
2024-04-09 16:03:37 +0200

    feat(node): Add telemetry and uptime

--- lib/modules/node/node-schema.ts
+++ lib/modules/node/node-schema.ts
@@ -11,3 +11,27 @@ export const Version = z.object({
   build_info: z.string(),
 });
 export type Version = z.infer<typeof Version>;
+
+export const Telemetry = z.object({
+  block_count: z.string(),
+  cemented_count: z.string(),
+  unchecked_count: z.string(),
+  account_count: z.string(),
+  bandwidth_cap: z.string(),
+  peer_count: z.string(),
+  protocol_version: z.string(),
+  uptime: z.string(),
+  genesis_block: z.string(),
+  major_version: z.string(),
+  minor_version: z.string(),
+  patch_version: z.string(),
+  pre_release_version: z.string(),
+  maker: z.string(),
+  timestamp: z.string(),
+  active_difficulty: z.string(),
+});
+export type Telemetry = z.infer<typeof Telemetry>;
+
+export const uptimeSchema = z.object({
+  seconds: z.string(),
+});
--- lib/modules/node/node.ts
+++ lib/modules/node/node.ts
@@ -1,7 +1,18 @@
 import { RpcConsummer } from '../rpc-consumer/rpc-consumer';
-import { Version } from './node-schema';
+import { Telemetry, Version, uptimeSchema } from './node-schema';
 
 export class Node extends RpcConsummer {
+  public async telemetry(): Promise<Telemetry> {
+    const res = await this.rpc.call('telemetry');
+    return this.parseHandler(res, Telemetry);
+  }
+
+  /// Return the number of seconds the node has been running
+  public async uptime(): Promise<string> {
+    const res = await this.rpc.call('uptime');
+    return this.parseHandler(res, uptimeSchema).seconds;
+  }
+
   public async version(): Promise<Version> {
     const res = await this.rpc.call('version');
     return this.parseHandler(res, Version);
