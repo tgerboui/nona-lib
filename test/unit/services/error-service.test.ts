334cd1dba26c4dd7b00956a2878c3970eba0396f
Thomas Gerbouin <thomasgerbouin@gmail.com>
2024-04-19 14:37:04 +0200

    test(error-service): add unit tests for ErrorService

---
+++ test/unit/services/error-service.test.ts
@@ -0,0 +1,43 @@
+import { AxiosError } from 'axios';
+import { ZodError, ZodIssue, ZodIssueCode } from 'zod';
+
+import { ErrorService } from '../../../lib/services/error/error-service';
+import { NonaNetworkError } from '../../../lib/shared/errors/network-error';
+import { NonaError } from '../../../lib/shared/errors/nona-error';
+import { NonaParseError } from '../../../lib/shared/errors/parse-error';
+
+describe('ErrorService', () => {
+  describe('handleError', () => {
+    it('should throw NonaError when error is an instance of NonaError', () => {
+      const error = new NonaError('Some error');
+      expect(() => ErrorService.handleError(error)).toThrow(NonaError);
+    });
+
+    it('should throw NonaNetworkError when error is an instance of AxiosError', () => {
+      const error = new AxiosError('Network error');
+      expect(() => ErrorService.handleError(error)).toThrow(NonaNetworkError);
+    });
+
+    it('should throw NonaParseError when error is an instance of ZodError', () => {
+      const zodIssue: ZodIssue = {
+        code: ZodIssueCode.invalid_type,
+        message: 'Invalid type',
+        expected: 'string',
+        received: 'number',
+        path: [],
+      };
+      const error = new ZodError([zodIssue]);
+      expect(() => ErrorService.handleError(error)).toThrow(NonaParseError);
+    });
+
+    it('should throw NonaError when error is an instance of Error', () => {
+      const error = new Error('Unknown error');
+      expect(() => ErrorService.handleError(error)).toThrow(NonaError);
+    });
+
+    it('should throw NonaError when error is of unknown type', () => {
+      const error = 'Some unknown error';
+      expect(() => ErrorService.handleError(error)).toThrow(NonaError);
+    });
+  });
+});
