ea1c07c62cbd54651770406c81218dbccbeba8f2
luxbe <luxbe@mailbox.org>
2024-04-27 13:00:13 +0200

    feat(nano name service): add NanoUsername type

--- lib/shared/utils/address.ts
+++ lib/shared/utils/address.ts
@@ -1,6 +1,14 @@
 import { z } from 'zod';
 
 /**
+ * a username starting with '@' which will be checked with the {@link NanoNameService}
+ */
+export type NanoUsername = `@${string}`;
+
+export const isValidNanoUsername = (address: string): address is NanoUsername =>
+  address.startsWith('@');
+
+/**
  * a Nano address starting with 'nano_'
  */
 export type NanoAddress = `nano_${string}`;
@@ -10,3 +18,8 @@ export const isValidNanoAddress = (address: string): address is NanoAddress =>
 
 export const zNanoAddress = (): z.ZodEffects<z.ZodString, NanoAddress, string> =>
   z.string().refine(isValidNanoAddress, "Address has to start with 'nano_'");
+
+/**
+ * Either a {@link NanoAddress} or a {@link NanoUsername}
+ */
+export type NanoTarget = NanoAddress | NanoUsername;
