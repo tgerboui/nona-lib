f04655290072db43ba53d1638a2a61fddb9199ee
luxbe <luxbe@mailbox.org>
2024-04-27 12:34:46 +0200

    refactor: add explicit return types and docs for NanoAddress

--- lib/shared/utils/address.ts
+++ lib/shared/utils/address.ts
@@ -1,9 +1,12 @@
 import { z } from 'zod';
 
+/**
+ * a Nano address starting with 'nano_'
+ */
 export type NanoAddress = `nano_${string}`;
 
 export const isValidNanoAddress = (address: string): address is NanoAddress =>
   address.startsWith('nano_');
 
-export const zNanoAddress = () =>
+export const zNanoAddress = (): z.ZodEffects<z.ZodString, NanoAddress, string> =>
   z.string().refine(isValidNanoAddress, "Address has to start with 'nano_'");
