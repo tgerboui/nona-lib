a86fd6f0bd2a86997f5b05acd09aedd65ac8dbce
Thomas Gerbouin <thomasgerbouin@gmail.com>
2024-05-16 15:47:40 +0200

    test(blocks): set realistic values for all cases

--- test/unit/modules/blocks.test.ts
+++ test/unit/modules/blocks.test.ts
@@ -40,27 +40,37 @@ describe('Blocks', () => {
   describe('create', () => {
     it('should hash and sign the block the the provided params and private key', async () => {
       const createParams: CreateBlockParams = {
-        account: 'nano_account',
-        previous: 'previousHash',
-        representative: 'nano_representative',
+        account: 'nano_1t7qkq38d8imy6hoin48ywxpokorxoh8kqz4zntxk6c4qkpsznhitda4a5wo',
+        previous: '572F88AD2B09D9A776BFF7D348FA18B302FC218C8DDD1FDCEA8F7B6F85C98552',
+        representative: 'nano_1tx1oxyczf9mczjnw8k8fktydjekry57aeet8f9cyxtpg9uf56owy4iup6ad',
         balance: '1000',
-        privateKey: 'privateKey',
-        link: 'link',
-        work: 'work',
+        privateKey: '684E8D92908E9A86E68EFCE6A53F723693DFFF8DB7D24CC1B92CF70AE46B8D00',
+        link: 'EF7C0B9747FAE586258DFC3674200C432E997C3991DEA7B33EF0097E67516316',
+        work: '2b3d689bbcb21dca',
       };
       const { privateKey, ...createParamsWithoutKey } = createParams;
 
-      blocks.hash = jest.fn().mockReturnValue('hash');
-      blocks.sign = jest.fn().mockReturnValue('signature');
+      blocks.hash = jest
+        .fn()
+        .mockReturnValue('C5390ECDDA5B0749EAEA1BCEC6B8CB36D10518E5A92E64A7DB59C26574EA1CC4');
+      blocks.sign = jest
+        .fn()
+        .mockReturnValue(
+          'B3889F33CBDEB33B4066C32D0D1A1506D37C699090ED19E9793E291BA2240236528DF95B52A1E2AE24BFB67BF348A0730D9B7EFB72350663BF7DA2B179D48F06',
+        );
 
       const result = blocks.create(createParams);
 
       expect(blocks.hash).toHaveBeenCalledWith(createParamsWithoutKey);
-      expect(blocks.sign).toHaveBeenCalledWith({ hash: 'hash', privateKey: 'privateKey' });
+      expect(blocks.sign).toHaveBeenCalledWith({
+        hash: 'C5390ECDDA5B0749EAEA1BCEC6B8CB36D10518E5A92E64A7DB59C26574EA1CC4',
+        privateKey: '684E8D92908E9A86E68EFCE6A53F723693DFFF8DB7D24CC1B92CF70AE46B8D00',
+      });
       expect(result).toEqual({
         ...createParamsWithoutKey,
         type: 'state',
-        signature: 'signature',
+        signature:
+          'B3889F33CBDEB33B4066C32D0D1A1506D37C699090ED19E9793E291BA2240236528DF95B52A1E2AE24BFB67BF348A0730D9B7EFB72350663BF7DA2B179D48F06',
       });
     });
   });
@@ -69,16 +79,20 @@ describe('Blocks', () => {
     it('should call process RPC method with the provided block and subtype, and parse the result', async () => {
       const block: Block['block'] = {
         type: 'state',
-        account: 'nano_address',
-        previous: 'previousHash',
-        representative: 'nano_representative',
+        account: 'nano_14oegg6hq5c1smq7hh1ht3m3sfc17d6u47tur6q5d8a6m7whjxgwr7mggaoo',
+        previous: 'C5390ECDDA5B0749EAEA1BCEC6B8CB36D10518E5A92E64A7DB59C26574EA1CC4',
+        representative: 'nano_3wmfn39dj8iz1hizuxe1jr6ggs1eqp3mpf9orj88hm8jtfap7epzgrn7x997',
         balance: '1000',
-        link: 'link',
-        link_as_account: 'nano_linkAsAccount',
-        signature: 'signature',
-        work: 'work',
+        link: 'A3B6E7535ABB4E765AB24E32639F1651BC05C77A67595031C061C6D7A6E947B2',
+        link_as_account: 'nano_3axpwxboogtggsfd6mjkeghjenfw1q5qnstsc1rw1rg8tymgkjxkg9wxbeun',
+        signature:
+          '0713BF2715CA7B5CA6F5AAB9082418B1F76BC7724A3CCDD269DD6FABD60B2F5C832127FB9E99B69E1C647BF8940547D3E7DD06CC1E22F388F14E52920DED8705',
+        work: '2b3d689bbcb21dca',
       };
-      const processResponse: Block = { block, hash: 'processedHash' };
+      const processResponse: Block = {
+        block,
+        hash: 'C2390ECDDA5B4749EAEA1BCEC6B8CB36D10518E5A92E64A7DB59C26574EA1CC4',
+      };
       const subtype = 'send';
       rpcCallMock.mockResolvedValue(processResponse);
 
@@ -97,25 +111,26 @@ describe('Blocks', () => {
     it('should call block_info RPC method with the provided hash and parse the result', async () => {
       const hash = 'blockHash';
       const infoResponse = {
-        block_account: 'nano_1t7qkq38d8imy6hoin48ywxpokorxoh8kqz4zntxk6c4qkpsznhitda4a5wo',
-        amount: 'amount',
-        balance: 'balance',
-        height: 'height',
+        block_account: 'nano_1ipx847tk8o46pwxt5qjdbncjqcbwcc1rrmqnkztrfjy5k7z4imsrata9est',
+        amount: '30000000000000000000000000000000000',
+        balance: '5606157000000000000000000000000000000',
+        height: '58',
         local_timestamp: '1713527539',
-        successor: 'successor',
-        confirmed: 'confirmed',
+        successor: '8D3AB98B301224253750D448B4BD997132400CEDD0A8432F775724F2D9821C72',
+        confirmed: 'true',
         contents: {
-          type: 'type',
-          account: 'nano_1t7qkq38d8imy6hoin48ywxpokorxoh8kqz4zntxk6c4qkpsznhitda4a5wo',
-          previous: 'previous',
-          representative: 'nano_1tx1oxyczf9mczjnw8k8fktydjekry57aeet8f9cyxtpg9uf56owy4iup6ad',
-          balance: 'balance',
-          link: 'link',
-          link_as_account: 'nano_3barjiyag1agq4akumc4hstgmjiqd5mhi81y5enkwh95srs76q4fy1j8u4ie',
-          signature: 'signature',
-          work: 'work',
+          type: 'state',
+          account: 'nano_1ipx847tk8o46pwxt5qjdbncjqcbwcc1rrmqnkztrfjy5k7z4imsrata9est',
+          previous: 'CE898C131AAEE25E05362F247760F8A3ACF34A9796A5AE0D9204E86B0637965E',
+          representative: 'nano_1stofnrxuz3cai7ze75o174bpm7scwj9jn3nxsn8ntzg784jf1gzn1jjdkou',
+          balance: '5606157000000000000000000000000000000',
+          link: '5D1AA8A45F8736519D707FCB375976A7F9AF795091021D7E9C7548D6F45DD8D5',
+          link_as_account: 'nano_1qato4k7z3spc8gq1zyd8xeqfbzsoxwo36a45ozbrxcatut7up8ohyardu1z',
+          signature:
+            '82D41BC16F313E4B2243D14DFFA2FB04679C540C2095FEE7EAE0F2F26880AD56DD48D87A7CC5DD760C5B2D76EE2C205506AA557BF00B60D8DEE312EC7343A501',
+          work: '8a142e07a10996d5',
         },
-        subtype: 'subtype',
+        subtype: 'send',
       };
       rpcCallMock.mockResolvedValue(infoResponse);
 
@@ -151,17 +166,23 @@ describe('Blocks', () => {
     });
 
     it('should hash using hashBlock from nanocurrency package', () => {
-      hashBlockMock.mockReturnValue('hash');
+      hashBlockMock.mockReturnValue(
+        '87434F8041869A01C8F6F263B87972D7BA443A72E0A97D7A3FD0CCC2358FD6F9',
+      );
 
-      const params = { previous: 'previous' };
+      const params = {
+        previous: 'FF0144381CFF0B2C079A115E7ADA7E96F43FD219446E7524C48D1CC9900C4F17',
+      };
       const result = blocks.hash(params as any);
 
       expect(hashBlock).toHaveBeenCalledWith(params);
-      expect(result).toBe('hash');
+      expect(result).toBe('87434F8041869A01C8F6F263B87972D7BA443A72E0A97D7A3FD0CCC2358FD6F9');
     });
 
     it('should convert previous if it is equal to 0', () => {
-      hashBlockMock.mockReturnValue('hash');
+      hashBlockMock.mockReturnValue(
+        '87434F8041869A01C8F6F263B87972D7BA443A72E0A97D7A3FD0CCC2358FD6F9',
+      );
 
       const params = { previous: '0' };
       const result = blocks.hash(params as any);
@@ -169,7 +190,7 @@ describe('Blocks', () => {
       expect(hashBlock).toHaveBeenCalledWith({
         previous: '0000000000000000000000000000000000000000000000000000000000000000',
       });
-      expect(result).toBe('hash');
+      expect(result).toBe('87434F8041869A01C8F6F263B87972D7BA443A72E0A97D7A3FD0CCC2358FD6F9');
     });
   });
 
@@ -180,11 +201,21 @@ describe('Blocks', () => {
     });
 
     it('should sign a block using signBlock from nanocurrency package', () => {
-      signBlockMock.mockReturnValue('signature');
+      signBlockMock.mockReturnValue(
+        '3BFBA64A775550E6D49DF1EB8EEC2136DCD74F090E2ED658FBD9E80F17CB1C9F9F7BDE2B93D95558EC2F277FFF15FD11E6E2162A1714731B743D1E941FA4560A',
+      );
 
-      const result = blocks.sign({ hash: 'hash', privateKey: 'privateKey' });
-      expect(signBlockMock).toHaveBeenCalledWith({ hash: 'hash', secretKey: 'privateKey' });
-      expect(result).toBe('signature');
+      const result = blocks.sign({
+        hash: 'FF0144381CFF0B2C079A115E7ADA7E96F43FD219446E7524C48D1CC9900C4F17',
+        privateKey: '696529D5C661226BC7AE674B05950A58F1954DADFD53B1ABF0AC633BF1EDA87A',
+      });
+      expect(signBlockMock).toHaveBeenCalledWith({
+        hash: 'FF0144381CFF0B2C079A115E7ADA7E96F43FD219446E7524C48D1CC9900C4F17',
+        secretKey: '696529D5C661226BC7AE674B05950A58F1954DADFD53B1ABF0AC633BF1EDA87A',
+      });
+      expect(result).toBe(
+        '3BFBA64A775550E6D49DF1EB8EEC2136DCD74F090E2ED658FBD9E80F17CB1C9F9F7BDE2B93D95558EC2F277FFF15FD11E6E2162A1714731B743D1E941FA4560A',
+      );
     });
   });
 });
